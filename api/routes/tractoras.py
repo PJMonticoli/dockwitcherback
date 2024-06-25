a6eef81bfba68fabd426d15b590589f5e9d0b040
PJMonticoli <pablo_monticoli@hotmail.com>
2024-06-25 09:32:49 -0300

    documento codigo para mayor entendimiento

--- api/routes/tractoras.py
+++ api/routes/tractoras.py
@@ -15,6 +15,9 @@ collection = get_collection('tractoras')
 def get_tractoras():
     try:
         tractoras = list(collection.find())
+        #  La función jsonify se utiliza para crear un objeto de respuesta de Flask con los datos JSON.
+        # json_util.dumps(tractoras) convierte la lista de documentos (tractoras)
+        # a una cadena JSON utilizando json_util de la biblioteca bson, que maneja el formato JSON extendido de MongoDB.
         return jsonify(json_util.dumps(tractoras)), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
